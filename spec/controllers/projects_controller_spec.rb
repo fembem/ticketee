0d6e93f7352f69395920f254b9e28f045714b210
Leo <leo.decandia@gmail.com>
2011-12-28 10:06:04 -1000

    project visibility permissions.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,6 +7,7 @@ describe ProjectsController do
   let(:project) { Factory(:project) }
   
   context "standard users" do
+    
     { "new" => "get",
     "create" => "post",
     "edit" => "get",
@@ -20,12 +21,20 @@ describe ProjectsController do
       end
     end
 
+    it "cannot access the show action" do
+      sign_in(:user, user)
+      get :show, :id => project.id
+      response.should redirect_to(projects_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+
+
   end
 
 
   
   it "displays an error for a missing project" do
-    
+    sign_in(:user, user)
     get :show, :id => "not-here"
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found."
