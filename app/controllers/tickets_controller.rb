ff6cfc42eb8866af6ad6c87dfc9a1c4c8b6e9eaf
Leo <leo.decandia@gmail.com>
2012-01-01 14:16:47 -1000

    3 attachments per ticket possible.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -40,6 +40,7 @@ before_filter :authorize_delete!, :only => :destroy
 
   def new
     @ticket = @project.tickets.build
+    3.times { @ticket.assets.build }
   end
 
   def create
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   
-  has_attached_file :asset
+  has_many :assets
+  accepts_nested_attributes_for :assets
   
 end
