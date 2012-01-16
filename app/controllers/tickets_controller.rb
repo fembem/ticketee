3ef362f4b09dee40a82a64f1ae6699a97df3904e
Leo <leo.decandia@gmail.com>
2012-01-16 10:51:13 -1000

    need permission to add tags.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -48,7 +48,9 @@ before_filter :authorize_delete!, :only => :destroy
     #@ticket = @project.tickets.build(params[:ticket])
     @ticket = @project.tickets.build(params[:ticket].merge!(:user => current_user))
     if @ticket.save
-      @ticket.tag!(params[:tags])
+      if can?(:tag, @project) || current_user.admin?
+        @ticket.tag!(params[:tags])
+      end
       flash[:notice] = "Ticket has been created."
       redirect_to [@project, @ticket]
     else
